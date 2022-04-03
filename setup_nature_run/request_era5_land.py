09377daff516d4de14a0d03c162052fd157f7d3e
Man-Yau Chan <chanmanyau@gmail.com>
2022-04-03 08:47:27 -0700

    starting to put together nature run

---
+++ setup_nature_run/request_era5_land.py
@@ -0,0 +1,31 @@
+''' Script to request for ERA-5 reanalysis data within a limited area for a limited time '''
+
+import cdsapi
+import numpy as np
+import datetime
+import sys
+
+date = datetime.datetime.strptime( sys.argv[1], '%Y%m%d%H%M')
+
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-single-levels',
+    {
+        'product_type':'reanalysis',
+        'format':'grib',
+        'variable': ['10m_u_component_of_wind','10m_v_component_of_wind','2m_dewpoint_temperature',
+                    '2m_temperature','land_sea_mask','mean_sea_level_pressure',
+                    'orography','sea_surface_temperature','skin_temperature',
+                    'soil_temperature_level_1','soil_temperature_level_2','soil_temperature_level_3',
+                    'soil_temperature_level_4','soil_type','standard_deviation_of_filtered_subgrid_orography',
+                    'surface_pressure','volumetric_soil_water_layer_1','volumetric_soil_water_layer_2',
+                    'volumetric_soil_water_layer_3','volumetric_soil_water_layer_4' ],
+        'area': [12.5, 52.5,-12.5,  92.5],
+        'year':'2011',
+        'month':date.strftime('%m'),
+        'day': date.strftime('%d'),
+        'time': date.strftime('%H')
+    },
+    'raw_era5/era5_land_' + date.strftime('%Y-%m-%d_%H')+'UTC.grib')
---
+++ setup_nature_run/request_era5_plvl.py
@@ -0,0 +1,34 @@
+''' Script to request for ERA-5 reanalysis data within a limited area for a limited time '''
+
+import cdsapi
+import numpy as np
+import datetime
+import sys
+
+date = datetime.datetime.strptime( sys.argv[1], '%Y%m%d%H%M')
+
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-pressure-levels',
+    {
+        'product_type':'reanalysis',
+        'format':'grib',
+        'variable':[  'geopotential','fraction_of_cloud_cover','ozone_mass_mixing_ratio','relative_humidity',
+                      'specific_cloud_ice_water_content','specific_cloud_liquid_water_content',\
+                       'specific_humidity','specific_rain_water_content','specific_snow_water_content',
+                       'temperature','u_component_of_wind','v_component_of_wind','vertical_velocity'
+        ],
+        'pressure_level':[ '1','2','3', '5','7','10','20','30','50', '70','100','125','150','175','200',
+                           '225','250','300','350','400','450','500','550','600','650','700','750',
+                           '775','800','825','850','875','900','925','950','975','1000' ],
+        'year': '2011',
+        'month': date.strftime( '%m' ),
+        'day': date.strftime( '%d' ),
+        'area': [12.5, 52.5,-12.5,  92.5],
+        'time': date.strftime( '%H' )
+        },
+   'raw_era5/era5_reanalysis_' + date.strftime('%Y-%m-%d_%H')+'UTC.grib')
+
+
